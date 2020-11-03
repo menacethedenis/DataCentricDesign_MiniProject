27719de3b1a5196e4ee512df4423c94e780e8f9c
menacethedenis <denislymer@gmail.com>
2020-11-03 02:24:35 +0000

    improved security settings

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:MongoAtlas20@myfirstcluster.zecjr.mongodb.net/task_manager?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', '')
 
 mongo = PyMongo(app)
 
